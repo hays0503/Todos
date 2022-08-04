dd121b4059bb477a374786026da9bc48b4f84a3c
hays hays0503 <cthtufgbv@mail.ru>
2022-08-05 01:22:09 +0600

    2

--- pgAdaptor.js
+++ pgAdaptor.js
@@ -9,7 +9,7 @@ const config = {
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  ssl: true,
+  ssl: false,
 };
 
 const db = pgp(config);
